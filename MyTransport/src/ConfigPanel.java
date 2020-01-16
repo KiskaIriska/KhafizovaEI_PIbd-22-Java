9c70c4f1595b706547448799cc9bce3142e782be
KiskaIriska <55252391+KiskaIriska@users.noreply.github.com>
2020-01-16 17:17:01 +0400

    Update ConfigPanel.java

--- MyTransport/src/ConfigPanel.java
+++ MyTransport/src/ConfigPanel.java
@@ -1,86 +1,53 @@
 import java.awt.Color;
-
 import java.awt.Graphics;
-
-
-
 import javax.swing.JPanel;
-
-
 
 public class ConfigPanel extends JPanel {
 	public ConfigPanel() {
 	}
-
+	
 	private ITransport transport;
 
 	private IGuns guns;
 
 	public ITransport getTransport() {
-
 		return transport;
-
 	}
 
 	public void setTransport(ITransport transport) {
-
 		this.transport = transport;
-
 	}
 
 	public IGuns getGuns() {
-
 		return guns;
-
 	}
 
 	public void setGuns(IGuns guns) {
-
 		this.guns = guns;
-
 	}
 
 	public void clearGuns() {
-
 		guns= null;
-
 	}
 
 	public void clear() {
-
 		transport = null;
-
 		guns = null;
-
 	}
 
 	public void paint(Graphics g) {
-
 		super.paint(g);
-
 		if (transport != null) {
-
 			transport.DrawAircraft(g);
 			setTransport(transport);
-
 			if (guns != null) {
-
 				g.setColor(Color.black);
-
 		       guns.setPosition(transport.getStartPosX(), transport.getStartPosY());
-
 		        guns.DrawAeroGuns(g, guns.getPositionX(), guns.getPositionY());
-
 			} else {
-
 				transport.DrawAircraft(g);
 				setTransport(transport);
-
 			}
-
 		}
-
 	}
-
-
 }
