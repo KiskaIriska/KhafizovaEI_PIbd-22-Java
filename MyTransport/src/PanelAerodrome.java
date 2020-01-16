a2d63e122ee06f7024f85e1b38d63956fca88b5b
KiskaIriska <55252391+KiskaIriska@users.noreply.github.com>
2020-01-16 16:52:29 +0400

    Update PanelAerodrome.java

--- MyTransport/src/PanelAerodrome.java
+++ MyTransport/src/PanelAerodrome.java
@@ -5,12 +5,9 @@ import javax.swing.JPanel;
 public class PanelAerodrome extends JPanel {
 
 	private Aerodrome<ITransport, IGuns> aerodrome;
-
 	public PanelAerodrome(Aerodrome<ITransport, IGuns> aerodrome) {
 		this.aerodrome = aerodrome;
 	}
-
-
 	public int AddSeveral(ITransport airplane, int count) {
 		return aerodrome.AddSeveral(airplane, count);
 	}
